a8eb9c323c692c77ac5321217e62d4ba6ef4a58f
Balajikrishnan GS <balajikrishnangs@ami.com>
2024-10-04 18:13:52 +0530

    optimized the previous duplicate ele for given value code

--- Array/src/duplicate_array.rs
+++ Array/src/duplicate_array.rs
@@ -2,14 +2,7 @@ pub struct Solution;
 
 impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>, val: i32) -> i32 {
-        let mut i = 0;
-        while i < nums.len() {
-            if nums[i] == val {
-                nums.remove(i);
-            } else {
-                i += 1;
-            }
-        }
+        nums.retain(|&x| x != val);
         nums.len() as i32
     }
 }
